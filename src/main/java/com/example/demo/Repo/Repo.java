9135bfaa393cb56d58931807e7332a1bde1ab0ab
Thomas <thomas.anastacio@gmail.com>
2020-05-23 21:44:28 +0200

    GIGIGIGI

--- src/main/java/com/example/demo/Repo/Repo.java
+++ src/main/java/com/example/demo/Repo/Repo.java
@@ -17,21 +17,21 @@ public class Repo {
     JdbcTemplate template;
 
     public List<Book> fetchAll(){
-        String sql = "SELECT * FROM thebookshop.books";
+        String sql = "SELECT * FROM BookStoreWebsite.Books";
         RowMapper<Book> rowMapper = new BeanPropertyRowMapper<>(Book.class);
         return template.query(sql, rowMapper);
     }
 
     public Book addBook (Book book){
 
-        String sql = "INSERT INTO thebookshop.books (BookId, AuthorFirstName, AuthorLastName, Title, ISBN) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO BookStoreWebsite.Books (BookId, AuthorFirstName, AuthorLastName, Title, ISBN) VALUES (?, ?, ?, ?, ?)";
         template.update(sql, book.getBookId(), book.getAuthorFirstName(), book.getAuthorLastName(), book.getTitle(), book.getISBN());
         return null;
     }
 
     public Book findBook (int bookId){
 
-        String sql = "SELECT * FROM thebookshop.books WHERE BookId = ?";
+        String sql = "SELECT * FROM BookStoreWebsite.Books WHERE BookId = ?";
         RowMapper<Book> rowMapper = new BeanPropertyRowMapper<>(Book.class);
         Book book = template.queryForObject(sql, rowMapper, bookId );
         return book;
@@ -39,20 +39,20 @@ public class Repo {
 
     public Boolean deleteBook (int bookId){
 
-        String sql = "DELETE FROM thebookshop.books WHERE BookId = ?";
+        String sql = "DELETE FROM BookStoreWebsite.Books WHERE BookId = ?";
         return template.update(sql, bookId)>0;
     }
 
     public Book updateBook (Book book, int bookId){
 
-        String sql = "UPDATE thebookshop.books SET AuthorFirstName = ?, AuthorLastName = ?, Title = ?, ISBN = ? WHERE BookId = ?";
+        String sql = "UPDATE BookStoreWebsite.Books SET AuthorFirstName = ?, AuthorLastName = ?, Title = ?, ISBN = ? WHERE BookId = ?";
         template.update(sql, book.getAuthorFirstName(), book.getAuthorLastName(), book.getTitle(), book.getISBN(), bookId);
         return null;
     }
 
     public Book findAnyBook (String keyword){
         //String sql = "SELECT * FROM BookStoreWebsite.Books WHERE BookId LIKE '%?%' OR AuthorFirstName LIKE '%?%' OR AuthorLastName LIKE '%?%' OR Title LIKE '%?%'OR ISBN LIKE '%?%' ";
-        String sql = "SELECT * FROM thebookshop.books WHERE BookId = ? OR AuthorFirstName = ? OR AuthorLastName = ? OR Title = ? OR ISBN = ? ";
+        String sql = "SELECT * FROM BookStoreWebsite.Books WHERE BookId = ? OR AuthorFirstName = ? OR AuthorLastName = ? OR Title = ? OR ISBN = ? ";
 
         //String sql = "SELECT * FROM whatever WHERE like %" +keystrock+  " % OR"
 
